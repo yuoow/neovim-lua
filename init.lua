5e5a19e595ea1d3b54f209503861cd091c79e204
yumoon <yumoon@live.com>
2022-03-25 19:29:36 +0800

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,90 @@
+-- 基础配置
+require("basic")
+-- 快捷键映射
+require("keybindings")
+-- Packer插件管理
+require("plugins")
+-- 主题设置
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+-- 内置LSP
+require("lsp.setup")
+require("lsp/cmp")
+require("lsp/ui")
+
+
+
+
+
+
+
+-- require("plugin-config.null-ls")
+-- require("plugin-config.formatter")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- require("utils.change-colorscheme")
+-- 输入法自动切换
+require("utils.im-select")
+-- 复制到windows剪贴板
+require("utils.fix-yank")
+
+--require("utils.global")
+-- require("plugin-config.nvim-treesitter")
+-- require("plugin-config.nvim-autopairs")
+-- require("plugin-config.comment")
+require("plugin-config.surround")
+-- require("plugin-config.nvim-colorizer")
+-- require("plugin-config.diffview")
+-- require("plugin-config.indent-blankline")
+-- require('plugin-config/feline')
+-- require("plugin-config.toggleterm")
+-- lua require('plugin-config/which-key')
+-- gitsigns
+-- require("plugin-config.gitsigns")
+-- 
+
+-- dap
+-- require("plugin-config.dap")
+-- require("plugin-config/rust-tools")
+-- require("plugin-config.dap-install")
+-- require("plugin-config.vimspector")
